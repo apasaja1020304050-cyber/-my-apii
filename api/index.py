87fb71cdeefdeb114f29d4dccc17c204ddefcd24
apasaja1020304050-cyber <apasaja1020304050@gmail.com>
2026-06-20 17:54:10 +0800

    index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,61 @@
 from http.server import BaseHTTPRequestHandler
+import json
+
+VALID_KEYS = [
+    "UCGGFREE111"
+]
 
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
+        if self.path == "/":
+            self.send_json({
+                "message": "Server Online"
+            })
+
+        else:
+            self.send_json({
+                "message": "Endpoint tidak ditemukan"
+            })
+
+
+    def do_POST(self):
+        length = int(self.headers.get("Content-Length", 0))
+        body = self.rfile.read(length)
+
+        try:
+            data = json.loads(body)
+        except:
+            data = {}
+
+        if self.path == "/api/check":
+            key = data.get("key")
+
+            self.send_json({
+                "valid": key in VALID_KEYS
+            })
+
+
+        elif self.path == "/api/check_trial":
+            key = data.get("trial_key")
+
+            self.send_json({
+                "valid": key in VALID_KEYS,
+                "duration": "Unlimited"
+            })
+
+
+        else:
+            self.send_json({
+                "message": "Endpoint tidak ditemukan"
+            })
+
+
+    def send_json(self, data):
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
 
         self.wfile.write(
-            b'{"message":"Server Online"}'
-      )
+            json.dumps(data).encode()
+                    )
