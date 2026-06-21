149f4d6d65b5153faa0314ff80bea9330966a623
apasaja1020304050-cyber <apasaja1020304050@gmail.com>
2026-06-21 09:35:00 +0800

    index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,19 @@
 from http.server import BaseHTTPRequestHandler
 import json
 import os
+from supabase import create_client
 
 VALID_KEYS = [
     "UCGGFREE111"
 ]
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
 
 class handler(BaseHTTPRequestHandler):
 
